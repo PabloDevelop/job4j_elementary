d11a6d01b0caa0b0e5570f6286e2c4f2ee932d80
pavelberzkaln <p.berzkaln@gmail.com>
2022-02-28 20:19:18 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/Greeting.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I love Java!";
+        System.out.println(idea);
+        int year = 2022;
+        System.out.println(idea + " But I am a newbie. " + year);
+    }
+}
