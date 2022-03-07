5836047821922b2ad099c3ed674a76a9d847d5ff
pavelberzkaln <p.berzkaln@gmail.com>
2022-03-07 10:00:26 +0300

    4.6.2. Switch - matcher [#334071 #272100]

--- src/test/java/ru/job4j/condition/MultipleSwitchWeekTest.java
+++ src/test/java/ru/job4j/condition/MultipleSwitchWeekTest.java
@@ -22,4 +22,100 @@ public class MultipleSwitchWeekTest {
         int out = MultipleSwitchWeek.numberOfDay(name);
         Assert.assertEquals(expected, out);
     }
+
+    @Test
+    public void numberOfDayTuesday() {
+        int expected = 2;
+        String name = "Tuesday";
+        int out = MultipleSwitchWeek.numberOfDay(name);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void numberOfDayVtornik() {
+        int expected = 2;
+        String name = "Вторник";
+        int out = MultipleSwitchWeek.numberOfDay(name);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void numberOfDayWednesday() {
+        int expected = 3;
+        String name = "Wednesday";
+        int out = MultipleSwitchWeek.numberOfDay(name);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void numberOfDaySreda() {
+        int expected = 3;
+        String name = "Среда";
+        int out = MultipleSwitchWeek.numberOfDay(name);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void numberOfDayThursday() {
+        int expected = 4;
+        String name = "Thursday";
+        int out = MultipleSwitchWeek.numberOfDay(name);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void numberOfDayChetverg() {
+        int expected = 4;
+        String name = "Четверг";
+        int out = MultipleSwitchWeek.numberOfDay(name);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void numberOfDayFriday() {
+        int expected = 5;
+        String name = "Friday";
+        int out = MultipleSwitchWeek.numberOfDay(name);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void numberOfDayPyatnica() {
+        int expected = 5;
+        String name = "Пятница";
+        int out = MultipleSwitchWeek.numberOfDay(name);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void numberOfDaySaturday() {
+        int expected = 6;
+        String name = "Saturday";
+        int out = MultipleSwitchWeek.numberOfDay(name);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void numberOfDaySubbota() {
+        int expected = 6;
+        String name = "Суббота";
+        int out = MultipleSwitchWeek.numberOfDay(name);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void numberOfDaySunday() {
+        int expected = 7;
+        String name = "Sunday";
+        int out = MultipleSwitchWeek.numberOfDay(name);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void numberOfDayVoskresene() {
+        int expected = 7;
+        String name = "Воскресенье";
+        int out = MultipleSwitchWeek.numberOfDay(name);
+        Assert.assertEquals(expected, out);
+    }
 }
